917735f186ad02b43978d4e5ca1ab66e01c9dd1f
sveta.kanani <sveta.kanani123@gmail.com>
2024-04-06 11:28:45 +0530

    Branch B 2 changes

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("In Test2 file");
+		System.out.println("Branch B line number 8 first change");
+		System.out.println("Branch B line number 8 second change");
 	}
 
 }
