4be00ea3062da22521cc507aef61797a8694ea29
sveta.kanani <sveta.kanani123@gmail.com>
2024-04-06 11:10:16 +0530

    resolve conflict

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -5,8 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("In Test1 file");
-		System.out.println("Line number 6 change In Test1 ");
 
+		System.out.println("Line number 6 change In Test1 ");
+		System.out.println("Branch B Line 7 change");
 	}
 
 }
